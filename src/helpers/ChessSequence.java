9eb6bfbe1d9ba18ec6f7c83c45e02a16d5e4e9a5
Xuxuh15 <xuxuhxv@gmail.com>
2025-02-15 23:02:49 +0100

    Fix method parameters and visibility. Adds a constructor.

---
+++ src/helpers/ChessSequence.java
@@ -0,0 +1,14 @@
+package helpers;
+
+/**
+ * Class that stores some common chess sequences to aid in testing 
+ */
+public class ChessSequence {
+	
+	
+	
+	public static final String[] LONDON_OPENING =  {
+	        "D2:D4", "G1:F3", "C1:F4", "E2:E3", "C2:C3", "H2:H3"
+	    };
+
+}
