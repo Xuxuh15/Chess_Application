42c4458679833f0bea703be4a863d1ec4e462a8d
Xuxuh15 <xuxuhxv@gmail.com>
2025-02-18 22:13:41 +0100

    Add class that applies different chess sequences to baord

--- src/helpers/ChessEmulator.java
+++ src/helpers/ChessEmulator.java
@@ -13,30 +13,38 @@ public class ChessEmulator {
 	 */
 	private ChessLogic logic; 
 	
-	
+	/**
+	 * Constructor.
+	 * @param logic chess logic
+	 */
+	public ChessEmulator(ChessLogic logic) {
+		this.logic = logic; 
+	}
 	/**
 	 * Takes an array representing a sequence of moves and applies sequence on chess board.
-	 * @param arr array representing a sequence of moves 
+	 * @param sequence array representing a sequence of moves 
 	 * @param board the chess board 
 	 * @throws ArrayIndexOutOfBoundsException if a move is out of bounds
 	 */
-	public void applySequence(Pair[] arr, ChessBoard board) throws ArrayIndexOutOfBoundsException {
+	public void applySequence(String[] sequence, ChessBoard board) throws ArrayIndexOutOfBoundsException {
+		
+		Pair[] arr = transformArray(sequence); 
 		
 		
-		for(int i = 0; i < arr.length; i*=2) {
+		for(int i = 0; i < arr.length; i+=2) {
 			Pair initPos = arr[i]; 
 			Pair destination = arr[i+1]; 
-			ChessPiece pieceToMove = board.checkSpace(initPos.row(), initPos.col()); 
+			ChessPiece pieceToMove = logic.peek(board, initPos);
 			logic.moveAndUpdate(board,pieceToMove, destination);
 		}
 	}
 	
 	/**
-	 * Takes in a array representing sequence of moves to be made e.g. [1A:1C, ....] and converts it into an array of Pair objects
+	 * Takes in a array representing sequence of moves to be made e.g. [1A:1C, ....] and converts it into an array of Pair objects.
 	 * @param movesToApply array with string representation of moves in form initialPostion:destination 
 	 * @return transformed array
 	 */
-	public Pair[] transformArray(String[] movesToApply) {
+	private Pair[] transformArray(String[] movesToApply) {
 		
 		Pair[] movesToApplyTransformed = new Pair[movesToApply.length * 2]; //since each pair is a starting position followed by destination, we need an array double original size
 		int index = 0; 
@@ -56,20 +64,23 @@ public class ChessEmulator {
 	}
 	
 	/**
-	 * Takes a string coordinate and transforms it into a Pair object
+	 * Takes a string coordinate and transforms it into a Pair object.
 	 * @param initPos a String representation of a coordinate e.g. 1A
 	 * @return a Pair object representing a coordinate on the board
 	 */
 	private Pair extractPos(String initPos) {
-		String[] rowCol = initPos.split(""); 
-		int row = Integer.parseInt(rowCol[0]); 
-		int col = logic.convertLettertoNum(rowCol[1]);
+		String[] colRow = initPos.split("");  
+		int row = Integer.parseInt(colRow[1]) - 1; 
+		int col = logic.convertLettertoNum(colRow[0]);
 		return new Pair(row, col); 
 		
 	}
 	
 	
-	
+	/**
+	 * Sets the chess logic.
+	 * @param logic chess logic.
+	 */
 	public void setLogic(ChessLogic logic) {
 		this.logic = logic; 
 	}
