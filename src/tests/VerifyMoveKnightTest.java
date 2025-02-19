ac1eeb2bcdb8389823bd25d604d8a653588f3f13
Xuxuh15 <xuxuhxv@gmail.com>
2025-02-19 18:01:00 +0100

    Add unit test for knight verification class

---
+++ src/tests/VerifyMoveKnightTest.java
@@ -0,0 +1,132 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import helpers.ChessEmulator;
+import helpers.Pair;
+import helpers.KnightChessSequences;
+import logic.ChessBoard;
+import logic.ChessLogic;
+import logic.ChessPiece;
+import logic.VerifyMoveKnight;
+import logic.ChessConstants;
+
+public class VerifyMoveKnightTest {
+	
+	private static ChessBoard board; 
+	private static ChessLogic logic; 
+	private static ChessPiece[] whitePieces; 
+	private static ChessPiece[] blackPieces; 
+	private static ChessEmulator emulator; 
+	private static VerifyMoveKnight verification; 
+
+	@BeforeAll
+	public static void setUpBeforeClass() throws Exception {
+		board = new ChessBoard(); 
+		logic = new ChessLogic(); 
+		whitePieces = logic.generatePieces(ChessConstants.WHITE); 
+		blackPieces = logic.generatePieces(ChessConstants.BLACK);
+		logic.setBlackPlayerChessArray(blackPieces);
+		logic.setWhitePlayerChessArray(whitePieces);
+		emulator = new ChessEmulator(logic);
+		verification = new VerifyMoveKnight(); 
+		
+		
+	}
+
+	@AfterAll
+	public static void tearDownAfterClass() throws Exception {
+		board = null; 
+		logic = null; 
+		 
+	}
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		logic = new ChessLogic(); 
+		whitePieces = logic.generatePieces(ChessConstants.WHITE); 
+		blackPieces = logic.generatePieces(ChessConstants.BLACK);
+		logic.setBlackPlayerChessArray(blackPieces);
+		logic.setWhitePlayerChessArray(whitePieces);
+		logic.setUpBoard(board);
+		emulator.setLogic(logic);
+		
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		board.resetBoard();
+		emulator.setLogic(null);
+		whitePieces = null; 
+		blackPieces= null; 
+	}
+	
+	
+	@Test
+	@DisplayName("verifyMoveKnight knight moves more than 2 spaces vertically throwsIllegalArgumentException")
+	public void verifyMoveKnight_MovesMoreThanTwoSpacesVertically_ThrowsIllegalArgumentException() { 
+		
+		ChessPiece knightToMove = logic.peek(board, new Pair(0,ChessConstants.G)); 
+		
+		assertThrows(IllegalArgumentException.class, ()->{
+			verification.verifyMove(board, knightToMove, new Pair(3,ChessConstants.F));
+		}, "Expected IllegalArgumentException to be thrown"); 
+		
+		
+	}
+	
+	@Test
+	@DisplayName("verifyMoveKnight knight moves more than 2 spaces horizontally throwsIllegalArgumentException")
+	public void verifyMoveKnight_MovesMoreThanTwoSpacesHorizontally_ThrowsIllegalArgumentException() {
+		
+		emulator.applySequence(KnightChessSequences.DEVELOP_WHITE_KNIGHT, board);
+		
+		ChessPiece knightToMove = logic.peek(board, new Pair(2,ChessConstants.F)); 
+		
+		assertThrows(IllegalArgumentException.class, ()->{
+			verification.verifyMove(board, knightToMove, new Pair(3,ChessConstants.C));
+		}, "Expected IllegalArgumentException to be thrown"); 
+		
+		
+	}
+	
+	@Test
+	@DisplayName("verifyMoveKnight knight does not move in L-seqeunce throwsIllegalArgumentException")
+	public void verifyMoveKnight_DoesNotMoveInL_ThrowsIllegalArgumentException() {
+		
+		ChessPiece knightToMove = logic.peek(board, new Pair(0,ChessConstants.G)); 
+		
+		assertThrows(IllegalArgumentException.class, ()->{
+			verification.verifyMove(board, knightToMove, new Pair(3,ChessConstants.G));
+		}, "Expected IllegalArgumentException to be thrown"); 
+		
+		
+	}
+	
+	@Test
+	@DisplayName("verifyKnight knight moves in L-Seqeunce returns true")
+	public void verifyMoveKnight_MovesInLSeqeunce_ReturnsTrue() {
+		ChessPiece knightToMove = logic.peek(board, new Pair(0,ChessConstants.G)); 
+		
+		assertEquals(true, verification.verifyMove(board, knightToMove, new Pair(2, ChessConstants.F)), 
+				"Expected true to be returned"); 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
